425b328e357e1f594d3fa0620485096084d8416a
Nikki138 <tayalnikita68@gmail.com>
2020-05-09 13:42:12 +0530

    commit2

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,31 +1,39 @@
-$(document).ready(function() {
+$(document).ready(function(){
 
-    /* For the sticky navigation*/
-   $('.js--section-features').waypoint(function(direction){
-       if(direction=="down") {
-           $('nav').addClass('sticky');
-       }
-       else{
-           $('nav').removeClass('sticky');
-       }
-   }, {
-  offset: '60px;'
-});
+    /*FOR THE STICKY NAVIGATION*/
+    $('.js--section-features').waypoint(function(direction){
 
-/* Scroll on Buttons*/
-    $('.js--scroll').click(function()
-                          {
-        $('html,body').animate({scrollTop: $('.js--plans').offset().top},1000);
+        if(direction == "down"){
+            $('nav').addClass('sticky');
+        }
+        else{
+            $('nav').removeClass('sticky');
+        }
+
+    }, {
+        offset: '60px'
     });
 
-    $('.js--scroll-to-start').click(function()
-                          {
-        $('html,body').animate({scrollTop: $('.js--section-features').offset().top},1000);
-    });
 
-    /* Navigation scroll*/
+    /*SCROLL ON BUTTONS*/
 
-    $('a[href*="#"]')
+
+    $('.js--scroll-to-plan').click(function(){
+
+        $('html,body').animate({scrollTop:$('.js--section-plans').offset().top},1000)
+
+    })
+
+    $('.js--scroll-to-start').click(function(){
+
+        $('html,body').animate({scrollTop:$('.js--section-features').offset().top},1000)
+
+    })
+
+
+    // Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#0"]')
   .click(function(event) {
@@ -60,29 +68,30 @@ $(document).ready(function() {
     }
   });
 
-    /* Animations on scroll*/
-
-    $('.js--wp-1').waypoint(function(direction){
+    /*ANIMATE ON SCROLL}*/
+    $('.js--wp-1').waypoint(function(direction) {
         $('.js--wp-1').addClass('animate__animated animate__fadeIn');
-    },{
+    }, {
         offset: '50%'
     });
 
-    $('.js--wp-2').waypoint(function(direction){
+    $('.js--wp-2').waypoint(function(direction) {
         $('.js--wp-2').addClass('animate__animated animate__fadeInUp');
-    },{
+    }, {
         offset: '50%'
     });
 
-    $('.js--wp-3').waypoint(function(direction){
-        $('.js--wp-3').addClass('aniamte__animated animate__fadeIn');
-    },{
+    $('.js--wp-3').waypoint(function(direction) {
+        $('.js--wp-3').addClass('animate__animated animate__fadeIn');
+    }, {
         offset: '50%'
     });
 
-    $('.js--wp-4').waypoint(function(direction){
+    $('.js--wp-4').waypoint(function(direction) {
         $('.js--wp-4').addClass('animate__animated animate__pulse');
-    },{
+    }, {
         offset: '50%'
     });
+
+
 });
